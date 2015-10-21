bf7b466e759546cc43057f463d725ea3c0c36db6
J Shin <jjshin85@gmail.com>
2015-10-21 17:59:07 -0500

    modify errors.rb

--- week-4/errors.rb
+++ week-4/errors.rb
@@ -9,56 +9,72 @@ cartmans_phrase = "Screw you guys " + "I'm going home."
 
 # This error was analyzed in the README file.
 # --- error -------------------------------------------------------
+# 1. What is the name of the file with the error?
+# errors.rb
+# 2. What is the line number where the error occurs?
+# line 8
+# 3. What is the type of error message?
+# Syntax error
+# 4. What additional information does the interpreter provide about this type of error?
+# It didnt' expect the '=', it expected it to be the end of input
+# 5. Where is the error in the code?
+# {Screw you guys " + "I'm going home." = cartmans_phrase}
+# 6. Why did the interpreter give you this error?
+# Because variables should be on the left side when declared
+
+false
 
 def cartman_hates(thing)
-  while true
     puts "What's there to hate about #{thing}?"
 end
+
 
 # This is a tricky error. The line number may throw you off.
 
 # 1. What is the name of the file with the error?
-# errors.rb
+#
 # 2. What is the line number where the error occurs?
-# 8
+# line 182, the last line of the file
 # 3. What is the type of error message?
-# syntax error
+# syntax error, unexpected end-of-input, expecting keyword_end
 # 4. What additional information does the interpreter provide about this type of error?
-# [unexpected '=', expecting end-of-input]
+# It was an unexpected end-of-input, and it was expecting keyword_end
 # 5. Where is the error in the code?
-# The original line was ["Screw you guys " + "I'm going home." = cartmans_phrase] and the interpreter didn't expect the '=' in the positio it was in.
+# The error is in the 'while true' part in the cartman_hates() method.
 # 6. Why did the interpreter give you this error?
-# The variable has to be declared on the left side, not the right side.
+# Because it doesn't specify when to stop, it can go on indefinitely.
 
 # --- error -------------------------------------------------------
 
-south_park
+south_park = "show"
+
+# 1. What is the line number where the error occurs?
+# line 49
+# 2. What is the type of error message?
+# undefined local variable or method 'south_park'
+# 3. What additional information does the interpreter provide about this type of error?
+# It says 'main:Object(NameError)', which I take as meaning it's in the main method, and it's an name error with an object
+# 4. Where is the error in the code?
+# 'south_park'
+# 5. Why did the interpreter give you this error?
+# It's an undefined variable
+
+# --- error -------------------------------------------------------
+
+def cartman()
+  puts "Hey guys"
+end
 
 # 1. What is the line number where the error occurs?
 #
 # 2. What is the type of error message?
-#
+# undefined local variable or method
 # 3. What additional information does the interpreter provide about this type of error?
-#
+# It said 'main:Object (NoMethodError)'
 # 4. Where is the error in the code?
-#
+# 'cartman()'
 # 5. Why did the interpreter give you this error?
-#
-
-# --- error -------------------------------------------------------
-
-cartman()
-
-# 1. What is the line number where the error occurs?
-#
-# 2. What is the type of error message?
-#
-# 3. What additional information does the interpreter provide about this type of error?
-#
-# 4. Where is the error in the code?
-#
-# 5. Why did the interpreter give you this error?
-#
+# The method is undefined
 
 # --- error -------------------------------------------------------
 
@@ -66,7 +82,7 @@ def cartmans_phrase
   puts "I'm not fat; I'm big-boned!"
 end
 
-cartmans_phrase("I hate Kyle")
+cartmans_phrase('I hate Kyle')
 
 # 1. What is the line number where the error occurs?
 #
@@ -168,4 +184,3 @@ require_relative "cartmans_essay.md"
 
 # --- REFLECTION -------------------------------------------------------
 # Write your reflection below as a comment.
-end
