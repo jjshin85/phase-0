a2a800bcfbfeb45cbae52d873ddcb25b7a204df4
J Shin <jjshin85@gmail.com>
2015-10-21 18:27:26 -0500

    Add calculate-grade directory and files

---
+++ week-4/calculate-grade/my_solution.rb
@@ -0,0 +1,22 @@
+# Calculate a Grade
+
+# I worked on this challenge [by myself, with: ].
+
+
+# Your Solution Below
+def get_grade(grade)
+
+  if grade <= 65
+      return "F"
+  elsif grade < 70
+      return "D"
+  elsif grade < 80
+      return "C"
+  elsif grade < 90
+      return "B"
+  else
+      return "A"
+  end
+end
+
+
