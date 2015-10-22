c8340f61731f6016114b11cedde774812c00914f
J Shin <jjshin85@gmail.com>
2015-10-21 22:11:05 -0500

    revise 4.3 challenges

--- week-4/address/my_solution.rb
+++ week-4/address/my_solution.rb
@@ -1,13 +1,14 @@
 # Format an Address
 
+
+# I worked on this challenge [by myself].
+
+
+# Your Solution Below
+
+
 def make_address(street, city, state, zip)
   output = "You live at #{street}, in the beautiful city of #{city}, #{state}. Your zip is #{zip}."
   puts output
   return output
 end
-
-
-# I worked on this challenge [by myself, with: ].
-
-
-# Your Solution Below
