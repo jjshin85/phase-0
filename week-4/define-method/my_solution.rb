6146f12d358a79c03da635d3f37888f523248e1a
J Shin <jjshin85@gmail.com>
2015-10-21 21:51:29 -0500

    revise my_solution.rb

--- week-4/define-method/my_solution.rb
+++ week-4/define-method/my_solution.rb
@@ -1,10 +1,11 @@
 # Define an Empty Method
-def do_something(string, string2, string3)
-  puts string + string2 + string3
-end
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 
 # Your Solution Below
+
+def do_something(string, string2, string3)
+  puts string + string2 + string3
+end
