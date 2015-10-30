0fa469f1c2b94f3de4b84d947d772408d0bcd1d8
J Shin <jjshin85@gmail.com>
2015-10-30 16:03:49 -0500

    add person 1 and 2's code to group-research-methods

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -5,33 +5,91 @@
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
 
-# Person 1's solution
-def my_array_finding_method(source, thing_to_find)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+# Research Methods
+
+#Pseudocode
+#Create a new empty array called has_key_letter
+#Test and return the strings with the key letter to the new empty array
+  #Test each word for the key letter using string method
+  #iterate over the array and run the method
+  #Add each key letter containing string to the new array
+  #end
+#End
+#Print has_key_letter array
+
+# Person 1's solution: Karla
+def my_array_finding_method(element_array, letter)
+  has_key_letter = Array.new
+   element_array.each do |element|
+    element = element.to_s
+    if element.include?(letter)
+      has_key_letter.push(element)
+    end
+   end
+   p has_key_letter
 end
 
-def my_hash_finding_method(source, thing_to_find)
-  source # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+my_array_finding_method(i_want_pets,"t")
+
+#Pseudocode
+#Create a new empty array called pet_is_age
+#Test and return the key's with a value matching the age input
+  #Test each word for the age value using the == method
+  #iterate over the array and run the method
+  #Add each key to the new array
+  #end
+#End
+#Print the pet_is_age array
+
+def my_hash_finding_method(hash_collection, age)
+   pet_is_age = []
+   hash_collection.each do |key,value|
+    if value == age
+      pet_is_age.push(key)
+    end
+   end
+   p pet_is_age
 end
 
+my_hash_finding_method(my_family_pets_ages, 3)
+
+=begin
+# Release 1: Identify and describe the Ruby method you implemented.
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+The unique ruby methods I used for my assignments were .push, include?, and .to_s.
+.push adds an element to an array.
+include? will check an element for the presence of a specified character. The one
+hitch with .include? is that it is a string method, meaning it will not run on
+elements of any other class. That is where the .to_s method came in. Because the
+array contains integers, we must turn them into strings before they can be successfully
+evaluated.
+=end
 
-# Person 2
+# Person 2: Nicola
+# pseudocode
+# input: name of array or hash you want to modify and number that adds to or replaces age data
+# output: modified array or hash with updated integer data
+# select integers in source array/hash
+# either add or replace integers with input
+# return same source array/hash that has been changed
+
 def my_array_modification_method!(source, thing_to_modify)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.map! {|x| x.is_a?(Integer) ? x + thing_to_modify : x}
+  return source
 end
 
 def my_hash_modification_method!(source, thing_to_modify)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.each { |pet, age| source[pet] = age + thing_to_modify }
+ return source
 end
 
 # Identify and describe the Ruby method(s) you implemented.
-#
-#
-#
+# map! - iterates over each element in the array and modifies the array so each element is changed
+#          according to the code in the block
+# is_a?(Integer) - this method checks each element in the array to see if it is an integer
+# I tried to use each_value for the hash modification, but couldn't figure out how to reassign
+# the age value without referencing the key. Let me know if anyone used that method to alter just the values.
+
 
 
 # Person 3
