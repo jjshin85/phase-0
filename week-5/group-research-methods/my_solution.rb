98feab22b0290dc050456978c74909b964682e7f
J Shin <jjshin85@gmail.com>
2015-11-01 21:59:02 -0600

    add person 4 solution

--- week-5/group-research-methods/my_solution.rb
+++ week-5/group-research-methods/my_solution.rb
@@ -1,6 +1,6 @@
 # Research Methods
 
-# I spent [.75] hours on this challenge.
+# I spent [1] hours on this challenge.
 
 i_want_pets = ["I", "want", 3, "pets", "but", "only", "have", 2]
 my_family_pets_ages = {"Evi" => 6, "Ditto" => 3, "Hoobie" => 3, "George" => 12, "Bogart" => 4, "Poly" => 4, "Annabelle" => 0}
@@ -120,15 +120,17 @@ end
 
 # Person 4
 def my_array_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.delete_if { |i| i.to_s.include? thing_to_delete }
+  return source
 end
 
 def my_hash_deletion_method!(source, thing_to_delete)
-  source.dup # This line is here to make sure all tests initially fail. Delete it when you begin coding.
+  source.delete_if { |key, value| key.include? thing_to_delete}
+  return source
 end
 
 # Identify and describe the Ruby method(s) you implemented.
-#
+# I used delete_if to iterate through the collections and see if any of the elements contained the string I was looking to delete.
 #
 #
 
